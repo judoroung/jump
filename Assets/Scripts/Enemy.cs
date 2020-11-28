7d0d33c0920c3ef15a3b73ba283383cceb2e4dbd
judoroung <72658509+judoroung@users.noreply.github.com>
2020-11-28 22:36:08 +0900

    add boss(Not complete)

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 1000;
+    public GameObject player;
+    public float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TakeDamage(int damage) {
+        health -= damage;
+
+    }
+    public void die() {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/weapon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public int damage = 10;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1")) {
+            Shoot();
+        }   
+    }
+
+    void Shoot() {
+        RaycastHit2D hitInfo =  Physics2D.Raycast(firePoint.position, firePoint.right);
+        if (hitInfo) {
+            Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+                if (enemy.health <= 0) {
+                    enemy.die();
+                }
+            }
+        }
+    }
+}
